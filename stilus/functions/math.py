cfc32bb0d67c21157ae0ae4b02d1e3f22cf0e20f
jw <jw@elevenbits.com>
2019-11-08 08:35:27 +0100

    Added tan test and bugfix.

--- stilus/functions/math.py
+++ stilus/functions/math.py
@@ -2,16 +2,15 @@ import math as m
 
 from stilus.nodes.string import String
 from stilus.nodes.unit import Unit
-from stilus.utils import assert_type, assert_string
+from stilus.utils import assert_type, assert_string, stilus_round
 
 
-# todo: convert all javascript math functions to python and vice versa
 def math(n: Unit, fn: String, evaluator=None):
     """Apply Math ``fn`` to ``n``"""
     assert_type(n, 'unit', 'n')
     assert_string(fn, 'fn')
 
     if fn.string == 'round':
-        return Unit(round(n.value), n.type)
+        return Unit(int(stilus_round(n.value)), n.type)
 
     return Unit(m.__getattribute__(fn.string)(n.value), n.type)
--- stilus/functions/tan.py
+++ stilus/functions/tan.py
@@ -1,8 +1,8 @@
 import logging
 
-from stilus.functions import math
+import math as m
 from stilus.nodes.unit import Unit
-from stilus.utils import assert_type
+from stilus.utils import assert_type, stilus_round
 
 log = logging.getLogger(__name__)
 
@@ -11,5 +11,15 @@ def tan(angle, evaluator=None):
     assert_type(angle, 'unit', 'angle')
     radians = angle.value
     if angle.type == 'deg':
-        radians *= math.pi / 180
-    return Unit(math.tan(radians), '')
+        radians = m.radians(radians)
+    pow = m.pow(10, 9)
+    sin = float(stilus_round(m.sin(radians) * pow)) / pow
+    cos = float(stilus_round(m.cos(radians) * pow)) / pow
+    if cos == 0:
+        tan = 'Infinity'
+    else:
+        tan = float(stilus_round(pow * sin / cos)) / pow
+    if tan == 0:
+        tan = 0
+    # return Unit(m.tan(radians), '')
+    return Unit(tan, '')
