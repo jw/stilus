c82f9522f0474a66fcb779a2441ab188576e9663
jw <jw@elevenbits.com>
2019-11-12 21:04:23 +0100

    Added stilus binary plain test.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,3 +7,13 @@ def test_cli_version():
     result = runner.invoke(cli.stilus, ['--version'])
     assert result.exit_code == 0
     assert result.output == __version__ + '\n'
+
+
+def test_cli_plain():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        with open('foo.styl', 'w') as f:
+            f.write('foo\n  abc def\n')
+        result = runner.invoke(cli.stilus, ['foo.styl'])
+        assert result.exit_code == 0
+        assert result.output == 'foo {\n  abc: def;\n}\n\n'
